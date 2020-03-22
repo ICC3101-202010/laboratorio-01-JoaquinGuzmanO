e5277492543fb8d8b7bd971a225f9308e5c32abc
Joaco Guzmán <jaguzman1@miuandes.cl>
2020-03-22 19:10:45 -0300

    casi listo

---
+++ constructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laboratorio_1
+{
+    class Class1
+    {       
+            Persona persona1 = new Persona("Bob", "Kunga");                     
+    }
+
+}
+

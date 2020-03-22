38c02a17e94ff87d53ca96b2e6ead3f107e239fa
Joaco Guzmán <jaguzman1@miuandes.cl>
2020-03-22 19:07:06 -0300

    lanzar listo

---
+++ Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace laboratorio_1
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona(string aNombre, string aApellido)
+        {
+            nombre = aNombre;
+            apellido = aApellido;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Lanzar());
+        }
+
+        static int Lanzar()
+        {
+            Random Num = new Random();
+            return Num.Next(0,3);
+        }
+    
+    }
+}
